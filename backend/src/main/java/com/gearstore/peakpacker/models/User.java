5dacbd3d7039f685c94d81ff50f5c74d05d777d5
Huyen Huynh <doraemonhip@gmail.com>
2023-08-22 20:41:49 -0400

    working on authentication/authorization

--- backend/src/main/java/com/gearstore/peakpacker/models/User.java
+++ backend/src/main/java/com/gearstore/peakpacker/models/User.java
@@ -5,10 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.*;
 
 @Entity
-@Table(name = "users") // specifies name of the table that mapped to User
+@Table(name = "users",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "emails")
+    }) // specifies name of the table that mapped to User
 @Getter
 @Setter
 public class User {
@@ -16,14 +23,36 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // tells JPA that id should be generated automatically
     private Long id;
 
+    @NotBlank
+    @Size(max = 20)
     @Column(unique = true, nullable = false)
     private String username;
 
-    @Column(nullable = false)
+    @NotBlank
+    @Size(max = 120)
     private String password;
 
+    @NotBlank
+    @Size(max = 50)
+    @Email
     @Column(unique = true, nullable = false)
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable( name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(){
+
+    }
+
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     @Column(nullable = false)
     private String firstName;
@@ -35,3 +64,15 @@ public class User {
     private List<Order> orders;
 
 }
+
+// @NotBlank use at application level to validate that a property of an entity is NOT NULL
+// and has length greater than zero when trimmed. If NULL or just whitespace, a violation will occur
+
+// @Column(nullable = false) is used at database level, if insert or update a record with a null value,
+// the database will throw an error.
+
+// If using only @NotBlank, then for some reason null value bypassed the application layer and hit
+// the database, if that column in db is not set to NOT NULL then the value can be saved to db
+
+// If using only @Column(nullable = false) then value like "   " can still be accepted into db because
+// it's technically NOT NULL value.
--- backend/src/main/java/com/gearstore/peakpacker/repositories/UserRepository.java
+++ backend/src/main/java/com/gearstore/peakpacker/repositories/UserRepository.java
@@ -1,11 +1,18 @@
 package com.gearstore.peakpacker.repositories;
 
+import java.util.Optional;
+
 import com.gearstore.peakpacker.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gearstore.peakpacker.models.User;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
 
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
 

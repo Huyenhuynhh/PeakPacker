b20b8e57d22839d33eb7a4231c66d1115a8355e1
Huyen Huynh <doraemonhip@gmail.com>
2023-08-30 01:33:37 -0400

    updated spring boot 3

--- backend/src/main/java/com/gearstore/peakpacker/PeakpackerApplication.java
+++ backend/src/main/java/com/gearstore/peakpacker/PeakpackerApplication.java
@@ -2,15 +2,8 @@ package com.gearstore.peakpacker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.gearstore.peakpacker.models")
-@EnableJpaRepositories(basePackages = "com.gearstore.peakpacker.repositories")
-@ComponentScan(basePackages = {"com.gearstore.peakpacker.security.services"})
-
 public class PeakpackerApplication {
 
 	public static void main(String[] args) {
